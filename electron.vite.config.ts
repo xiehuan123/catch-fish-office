86eb3bbe2a26794db7de3ae8f22cb96558f2239b
xiehuan123 <1208044257@qq.com>
2025-03-14 00:34:04 +0800

    ok

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin, bytecodePlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
-import postCssPxToRem from "postcss-pxtorem";
+import postCssPxToRem from 'postcss-pxtorem'
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin(), bytecodePlugin()]
@@ -16,22 +16,21 @@ export default defineConfig({
       }
     },
     plugins: [vue()],
-     css: {
-    postcss: {
-      plugins: [
-        postCssPxToRem({
-          // 配置在将px转化为rem时 1rem等于多少px(因为我们搭配使用了amfe-flexible插件 此处我们需要设置的值应是UI设计稿全屏基准宽度的十分之一)
-          // 当UI设计稿的全屏基准宽度是1920px时 此处设置的值为192
-          rootValue: 192,
-          // 所有px均转化为rem
-          propList: ["*"]
-          // 若想设置部分样式不转化 可以在配置项中写出
-          // eg: 除 border和font-size外 所有px均转化为rem
-          // propList: ["*", "!height",], 
-        })
-      ]
+    css: {
+      postcss: {
+        plugins: [
+          postCssPxToRem({
+            // 配置在将px转化为rem时 1rem等于多少px(因为我们搭配使用了amfe-flexible插件 此处我们需要设置的值应是UI设计稿全屏基准宽度的十分之一)
+            // 当UI设计稿的全屏基准宽度是1920px时 此处设置的值为192
+            rootValue: 192,
+            // 所有px均转化为rem
+            propList: ['*']
+            // 若想设置部分样式不转化 可以在配置项中写出
+            // eg: 除 border和font-size外 所有px均转化为rem
+            // propList: ["*", "!height",],
+          })
+        ]
+      }
     }
-
-  },
   }
 })
